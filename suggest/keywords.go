9b2b33dc2d46a7735400ba7c5c808f9f79edcd07
remeh <me@remy.io>
2017-08-01 19:05:37 +0200

    suggest: load user keywords.

--- suggest/keywords.go
+++ suggest/keywords.go
@@ -2,12 +2,28 @@ package suggest
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/remeh/smartwitter/log"
 	"github.com/remeh/smartwitter/storage"
 	"github.com/remeh/uuid"
 
 	"github.com/lib/pq"
 )
+
+type Keywords []keywords
+type keywords struct {
+	Uid          uuid.UUID `json:"-"`
+	UserUid      uuid.UUID `json:"-"`
+	Keywords     []string  `json:"keywords"`
+	Position     int       `json:"position"`
+	CreationTime time.Time `json:"creation_time"`
+	LastUpdate   time.Time `json:"last_update"`
+}
+
+func (k Keywords) Len() int               { return len(k) }
+func (k Keywords) Less(i int, j int) bool { return k[i].Position < k[j].Position }
+func (k Keywords) Swap(i int, j int)      { k[i], k[j] = k[j], k[j] }
 
 // SetKeywords stores the given keywords for the user.
 // It also resets the last run on these, forcing a near-to-be executed
@@ -30,4 +46,44 @@ func SetKeywords(userUid uuid.UUID, keywords []string, position int) error {
 	}
 
 	return nil
+}
+
+// GetKeywords returns users watched keywords.
+func GetKeywords(userUid uuid.UUID) (Keywords, error) {
+	rv := make(Keywords, 0)
+
+	if userUid.IsNil() {
+		log.Warning("GetKeywords called with a nil userUid")
+		return rv, nil
+	}
+
+	rows, err := storage.DB().Query(`
+		SELECT "uid", "keywords", "position", "creation_time", "last_update"
+		FROM
+			"twitter_keywords_watcher"
+		WHERE
+			"user_uid" = $1
+		ORDER BY "position"
+	`, userUid)
+	if err != nil {
+		return rv, log.Err("GetKeywords", err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		k := keywords{}
+		if err := rows.Scan(
+			&k.Uid,
+			pq.Array(&k.Keywords),
+			&k.Position,
+			&k.CreationTime,
+			&k.LastUpdate,
+		); err != nil {
+			return rv, log.Err("GetKeywords", err)
+		}
+		k.UserUid = userUid
+		rv = append(rv, k)
+	}
+	return rv, nil
 }
